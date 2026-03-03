a9f80e5a3cb20d2b72752916ec30726d7f451991
Joel <joelclaude0817@gmail.com>
2026-03-03 18:02:38 +0800

    Add installation script for PyEdit

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo "========================"
+echo " PyEdit - An Code Editor"   
+echo "========================"
+echo "Installing Python..."
+apt install python
+echo "Changing Directories..."
+cd PyEdit
+echo "Done! Now Execute 'python pyedit.py' to run the program."
